5ef093b36d1e3dc05025ab66d395ffbca80782b2
co0lc0der <c0der@ya.ru>
2022-04-09 13:03:03 +0500

    refactor CardToolsHelper

--- src/CardToolsHelper.php
+++ src/CardToolsHelper.php
@@ -7,167 +7,190 @@ namespace co0lc0der\Lte3Widgets;
  */
 class CardToolsHelper
 {
+	const DEFAULT_ICON = 'circle';
+	const CREATE_TITLE = 'Create';
+	const PROFILE_TITLE = 'Profile';
+	const UPDATE_TITLE = 'Update';
+	const DELETE_CONFIRM_TEXT = 'Do you really want to delete this?';
+	const COLLAPSE_TITLE = 'Collapse/Restore';
+	const EXPAND_TITLE = 'Maximize';
+	const CLOSE_TITLE = 'Close';
+	const REFRESH_TITLE = 'Refresh';
+	const CONTACTS_TITLE = 'Contacts';
+
 	/**
-	 * @param string $title
-	 * @param array $options
+	 * @param string $icon FontAwesone icon (without 'fa-' prefix)
+	 * @param string $title Title of a button and 'title' attribute
+	 * @param array $options Yii standard options for HTML tag
 	 * @return array
 	 */
-	public static function createButton(string $title = '', array $options = []): array
+	public static function button(string $icon = self::DEFAULT_ICON, string $title = '', array $options = []): array
 	{
 		return [
-			'link',
-			($title) ? "<i class=\"fas fa-plus mr-2\"></i>{$title}" : "<i class=\"fas fa-plus\"></i>",
-			['create'],
+			'button',
+			($title) ? "<i class=\"fas fa-{$icon} mr-2\"></i>{$title}" : "<i class=\"fas fa-{$icon}\"></i>",
 			array_merge([
-				'class' => 'btn btn-tool bg-success',
 				'title' => $title,
 			], $options),
 		];
 	}
 
 	/**
-	 * @param int $id
-	 * @param string $title
-	 * @param array $options
+	 * @param string|array $url Yii standard URL
+	 * @param string $icon FontAwesone icon (without 'fa-' prefix)
+	 * @param string $title Title of a link and 'title' attribute
+	 * @param array $options Yii standard options for HTML tag
 	 * @return array
 	 */
-	public static function profileButton(int $id, string $title = '', array $options = []): array
+	public static function a($url = '#', string $icon = self::DEFAULT_ICON, string $title = '', array $options = []): array
 	{
 		return [
 			'link',
-			($title) ? "<i class=\"fas fa-id-card mr-2\"></i>{$title}" : "<i class=\"fas fa-id-card\"></i>",
-			['profile', 'id' => $id],
+			($title) ? "<i class=\"fas fa-{$icon} mr-2\"></i>{$title}" : "<i class=\"fas fa-{$icon}\"></i>",
+			$url,
 			array_merge([
-				'class' => 'btn btn-tool bg-primary',
 				'title' => $title,
 			], $options),
 		];
 	}
 
 	/**
-	 * @param int $id
-	 * @param string $title
-	 * @param array $options
+	 * @param string $title Title of a badge and 'title' attribute
+	 * @param array $options Yii standard options for HTML tag
 	 * @return array
 	 */
-	public static function updateButton(int $id, string $title = '', array $options = []): array
+	public static function label(string $title, array $options = []): array
 	{
 		return [
-			'link',
-			($title) ? "<i class=\"fas fa-pencil-alt mr-2\"></i>{$title}" : "<i class=\"fas fa-pencil-alt\"></i>",
-			['update', 'id' => $id],
+			'label',
+			$title,
 			array_merge([
-				'class' => 'btn btn-tool bg-warning',
 				'title' => $title,
 			], $options),
 		];
 	}
 
 	/**
-	 * @param int $id
-	 * @param string $title
-	 * @param array $options
+	 * @param string $title Title of a link and 'title' attribute
+	 * @param array $options Yii standard options for HTML tag
+	 * @return array
+	 */
+	public static function createButton(string $title = self::CREATE_TITLE, array $options = []): array
+	{
+		return CardToolsHelper::a('create', 'plus', $title, array_merge([
+			'class' => 'btn btn-tool bg-success',
+		], $options));
+	}
+
+	/**
+	 * @param int $id User's ID for a profile link
+	 * @param string $title Title of a link and 'title' attribute
+	 * @param array $options Yii standard options for HTML tag
+	 * @return array
+	 */
+	public static function profileButton(int $id, string $title = self::PROFILE_TITLE, array $options = []): array
+	{
+		return CardToolsHelper::a(['profile', 'id' => $id], 'id-card', $title, array_merge([
+			'class' => 'btn btn-tool bg-primary',
+		], $options));
+	}
+
+	/**
+	 * @param int $id ID for an update link
+	 * @param string $title Title of a link and 'title' attribute
+	 * @param array $options Yii standard options for HTML tag
+	 * @return array
+	 */
+	public static function updateButton(int $id, string $title = self::UPDATE_TITLE, array $options = []): array
+	{
+		return CardToolsHelper::a(['update', 'id' => $id], 'pencil-alt', $title, array_merge([
+			'class' => 'btn btn-tool bg-warning',
+		], $options));
+	}
+
+	/**
+	 * @param int $id ID for a delete link
+	 * @param string $title Title of a link and 'title' attribute
+	 * @param array $options Yii standard options for HTML tag
 	 * @return array
 	 */
 	public static function deleteButton(int $id, string $title = '', array $options = []): array
 	{
-		return [
-			'link',
-			($title) ? "<i class=\"fas fa-trash mr-2\"></i>{$title}" : "<i class=\"fas fa-trash\"></i>",
-			['delete', 'id' => $id],
-			array_merge([
-				'class' => 'btn btn-tool bg-danger',
-				'title' => $title,
-				'data' => [
-					'confirm' => 'Do you really want to delete this?',
-					'method' => 'post',
-				],
-			], $options),
-		];
+		return CardToolsHelper::a(['delete', 'id' => $id], 'trash', $title, array_merge([
+			'class' => 'btn btn-tool bg-danger',
+			'data' => [
+				'confirm' => self::DELETE_CONFIRM_TEXT,
+				'method' => 'post',
+			],
+		], $options));
 	}
 
 	/**
-	 * @param bool $hide
-	 * @param array $options
+	 * @param bool $hide Is this collapsed card?
+	 * @param string $title 'title' attribute of a button
+	 * @param array $options Yii standard options for HTML tag
 	 * @return array
 	 */
-	public static function collapseButton(bool $hide = false, array $options = []): array
+	public static function collapseButton(bool $hide = false, string $title = self::COLLAPSE_TITLE, array $options = []): array
 	{
-		return [
-			'button',
-			($hide) ? '<i class="fas fa-plus"></i>' : '<i class="fas fa-minus"></i>',
-			array_merge([
-				'data-card-widget' => 'collapse',
-				'title' => 'Collapse/Restore',
-			], $options),
-		];
+		return CardToolsHelper::button(($hide) ? 'plus' : 'minus', '', array_merge([
+			'data-card-widget' => 'collapse',
+			'title' => $title,
+		], $options));
 	}
 
 	/**
-	 * @param array $options
+	 * @param string $title 'title' attribute of a button
+	 * @param array $options Yii standard options for HTML tag
 	 * @return array
 	 */
-	public static function expandButton(array $options = []): array
+	public static function expandButton(string $title = self::EXPAND_TITLE, array $options = []): array
 	{
-		return [
-			'button',
-			'<i class="fas fa-expand"></i>',
-			array_merge([
-				'data-card-widget' => 'maximize',
-				'title' => 'Maximize',
-			], $options),
-		];
+		return CardToolsHelper::button('expand', '', array_merge([
+			'data-card-widget' => 'maximize',
+			'title' => $title,
+		], $options));
 	}
 
 	/**
-	 * @param array $options
+	 * @param string $title 'title' attribute of a button
+	 * @param array $options Yii standard options for HTML tag
 	 * @return array
 	 */
-	public static function closeButton(array $options = []): array
+	public static function closeButton(string $title = self::CLOSE_TITLE, array $options = []): array
 	{
-		return [
-			'button',
-			'<i class="fas fa-times"></i>',
-			array_merge([
-				'data-card-widget' => 'remove',
-				'title' => 'Close',
-			], $options),
-
-		];
+		return CardToolsHelper::button('times', '', array_merge([
+			'data-card-widget' => 'remove',
+			'title' => $title,
+		], $options));
 	}
 
 	/**
-	 * @param string $source
-	 * @param array $options
+	 * @param string $source URL for getting data
+	 * @param string $title 'title' attribute of a button
+	 * @param array $options Yii standard options for HTML tag
 	 * @return array
 	 */
-	public static function refreshButton(string $source = '#', array $options = []): array
+	public static function refreshButton(string $source = '#', string $title = self::REFRESH_TITLE, array $options = []): array
 	{
-		return [
-			'button',
-			'<i class="fas fa-sync-alt"></i>',
-			array_merge([
-				'data-source' => $source,
-				'data-source-selector' => '#card-refresh-content',
-				'data-card-widget' => 'card-refresh',
-				'title' => 'Refresh',
-			], $options),
-		];
+		return CardToolsHelper::button('sync-alt', '', array_merge([
+			'data-source' => $source,
+			'data-source-selector' => '#card-refresh-content',
+			'data-card-widget' => 'card-refresh',
+			'title' => $title,
+		], $options));
 	}
 
 	/**
-	 * @param array $options
+	 * @param string $title 'title' attribute of a button
+	 * @param array $options Yii standard options for HTML tag
 	 * @return array
 	 */
-	public static function contactsButton(array $options = []): array
+	public static function contactsButton(string $title = self::CONTACTS_TITLE, array $options = []): array
 	{
-		return [
-			'button',
-			'<i class="fas fa-comments"></i>',
-			array_merge([
-				'data-widget' => 'chat-pane-toggle',
-				'title' => 'Contacts',
-			], $options),
-		];
+		return CardToolsHelper::button('comments', '', array_merge([
+			'data-widget' => 'chat-pane-toggle',
+			'title' => $title,
+		], $options));
 	}
 }
