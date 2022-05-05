bbf2b7044dbb6e2413fb5c3686e17843f119e4ee
co0lc0der <c0der@ya.ru>
2022-05-05 23:36:54 +0500

    add CustomCssSupportTrait

---
+++ src/CustomCssSupportTrait.php
@@ -0,0 +1,30 @@
+<?php
+namespace co0lc0der\Lte3Widgets;
+
+/**
+ * Trait CustomCssSupportTrait
+ * @package co0lc0der\Lte3Widgets
+ */
+trait CustomCssSupportTrait
+{
+	/**
+	 * additional CSS classes
+	 * format: [
+	 *  0 => 'classes-for-card-wrapper',
+	 *  1 => 'classes-for-card-header',
+	 *  2 => 'classes-for-card-title',
+	 *  3 => 'classes-for-card-body',
+	 *  4 => 'classes-for-card-footer',
+	 * ]
+	 * @var array|string[]
+	 */
+	public array $cssClasses = [];
+
+	/**
+	 * @return string
+	 */
+	protected function getCustonCssClass(int $index): string
+	{
+		return (isset($this->cssClasses[$index]) && !empty($this->cssClasses[$index])) ? " {$this->cssClasses[$index]}" : '';
+	}
+}
